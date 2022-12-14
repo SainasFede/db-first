efbc162cdcda392483f6729d51118e6bc36ace53
SainasFede <sainasfede@gmail.com>
2022-12-14 16:21:51 +0100

    add img

---
+++ auto.sql
@@ -0,0 +1,15 @@
+id - INT, NOT NULL, PRIMARY KEY
+brand - CHAR(25), NOT NULL
+model - CHAR(25), NOT NULL
+motor - CHAR(25), NOT NULL
+displacement - CHAR(20), NULL
+power_suply - CHAR(25), NULL
+passenger_seats - SMALLINT(), NULL
+color - CHAR(30), NULL
+year - YEAR, NULL
+plot - TEXT, NULL
+is_available - TINYINT, NOT NULL DEFAULT
+condition - VARCHAR(20), NOT NULL
+quantity - TINYINT, NOT NULL, DEFAULT
+poster - VARCHAR(), NULL
+note - VARCHAR(), NULL
